0bdd366579435be1e383b16ebdfb0ec552d59ea2
Alain Alisca <alainalisca@gmail.com>
2022-12-14 05:48:45 -0500

    commit tree 6421a9d8b71078284afeb1c5f3fbe1335dce11e4

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let ride;
+   if (distance <= 400) {
+       return ride = "This one is on me!"; 
+    } else if (distance >= 2000 && distance <= 2500) {
+        return ride = "I will gladly take your thirty bucks.";
+      } else (distance >= 2501) 
+      {
+          return ride = "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let location
+  location = city === "NYC" ? "Ok, sounds good." : "No go."  
+  return location
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let generous
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
